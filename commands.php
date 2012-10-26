bfbf923cfe58a3eb69b537d357b8035ee157f5e5
Mahmoud Saada <mahmoudsaada@gmail.com>
2012-10-26 16:52:11 -0700

    Quick Test

--- commands.php
+++ commands.php
@@ -1,36 +1,36 @@
 <?php 
-	
-	if(isset($_POST['action']) && !empty($_POST['action'])) {
-	    $action = $_POST['action'];
-	    switch($action) {
-	        case 'addGraph' :
-	        {
-	        	if(isset($_POST['name']) && !empty($_POST['name'])
-	        		&& isset($_POST['xml']) && !empty($_POST['xml'])
-        		{
-        			if(addGraph($_POST['name'],$_POST['xml']) != null);
-        				return true
-        		}
-	        	break;
-	        }
-	    }
-	}
-	else
-	{
-		return "Error: Could not execute your request!";
-	}
+	echo "HELLO!";exit();
+	// if(isset($_REQUEST['action']) && !empty($_REQUEST['action'])) {
+	//     $action = $_REQUEST['action'];
+	//     switch($action) {
+	//         case 'addGraph' :
+	//         {
+	//         	if(isset($_REQUEST['name']) && !empty($_REQUEST['name'])
+	//         		&& isset($_REQUEST['xml']) && !empty($_REQUEST['xml'])
+ //        		{
+ //        			if(addGraph($_REQUEST['name'],$_REQUEST['xml']) != null);
+ //        				return true
+ //        		}
+	//         	break;
+	//         }
+	//     }
+	// }
+	// else
+	// {
+	// 	return "Error: Could not execute your request!";
+	// }
 
-	//Require the ActiveRecord class
-	require_once 'ActiveRecord/php-activerecord/ActiveRecord.php';
+	// //Require the ActiveRecord class
+	// require_once 'ActiveRecord/php-activerecord/ActiveRecord.php';
 	
-	//Set the database configuration and connection
-	include('ActiveRecord/Configuration.php');
+	// //Set the database configuration and connection
+	// include('ActiveRecord/Configuration.php');
 	
-	//Include models
-	include('ActiveRecord/models/GraphModel.php');
+	// //Include models
+	// include('ActiveRecord/models/GraphModel.php');
 
-	protected function addGraph(name, xml)
-	{
-		return Graph::create(array('name'=>,'content'=>));
-	}
+	// protected function addGraph(name, xml)
+	// {
+	// 	return Graph::create(array('name'=>,'content'=>));
+	// }
 ?>
