342d508b3497af3c3b71e32bb7aa8314b590f6e4
mobicloud <mobicloud@mobicloud.asu.edu>
2012-10-26 05:01:22 -0700

    Init with canvas files

---
+++ commands.php
@@ -0,0 +1,12 @@
+<?php 
+	// if(isset($_POST['action']) && !empty($_POST['action'])) {
+	//     $action = $_POST['action'];
+	//     switch($action) {
+	//         case 'addGraph' : test();break;
+	//         case 'remove' : blah();break;
+	//         case 'update'
+	//         // ...etc...
+	//     }
+	// }
+	return true;
+?>
---
+++ graphLayout.php
@@ -0,0 +1,47 @@
+
+<?php
+	//Require the ActiveRecord class
+	require_once 'ActiveRecord/php-activerecord/ActiveRecord.php';
+	
+	//Set the database configuration and connection
+	include('ActiveRecord/Configuration.php');
+	
+	//Include models
+	include('ActiveRecord/models/GraphModel.php');
+
+	//display in order
+	showWelcomeMessage();
+	showGraph("100%","80%");
+
+	function showWelcomeMessage(){
+		global $user;
+		if ($user->uid) {
+			$greeting = 'Welcome '.format_username($user).' to the VLAB playground!';
+		}
+		else {
+			$greeting = 'Welcome visitor to the VLAB playground!';
+		}
+		echo "<h1>".$greeting."</h1>";
+	}
+
+	function showGraph($width,$height){
+		echo '<iframe id="myGraph" src="https://vlab.asu.edu/canvas/canvas/graph.php?lab_id=548" '
+			.'width="'.$width.'" height="'.$height.'"></iframe>';
+	}
+
+	// Sample create command using ActiveRecord:
+	// $graph = Graph::create(array('name'=>,'content'=>));
+?>
+
+
+<div id="graphMenu">
+	<div class="row">
+		<select id="graphList">
+    	</select>​
+	</div>
+</div>
+
+<script type="text/javascript">
+	jQuery(document).ready(function(){
+	});
+</script>
