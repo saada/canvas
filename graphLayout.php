d8bae8c6b093431d941566dca1ff7b42493324e5
saada <mahmoudsaada@gmail.com>
2012-11-02 11:20:17 -0700

    Test json return value

--- graphLayout.php
+++ graphLayout.php
@@ -36,3 +36,23 @@
 	jQuery(document).ready(function(){
 	});
 </script>
+
+<?php 
+	//Require the ActiveRecord class
+	require_once 'ActiveRecord/php-activerecord/ActiveRecord.php';
+	
+	//Set the database configuration and connection
+	include('ActiveRecord/Configuration.php');
+	
+	//Include models
+	include('ActiveRecord/models/GraphModel.php');
+
+	function addGraph($name, $xml)
+	{	
+		$result = Graph::create(array('name'=>$name,'content'=>$xml));
+		echo json_encode($result);
+		return $result;
+	}
+
+	print_r(addGraph("Jin", "Hui"));
+ ?>
